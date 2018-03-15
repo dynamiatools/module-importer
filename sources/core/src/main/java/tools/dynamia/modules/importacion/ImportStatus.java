5ec5414a1601666eb48718b084d6b6d736c625d9
Mario Serrano <mario@dynamiasoluciones.com>
2018-03-15 17:02:21 -0500

    minor improvements

---
+++ sources/core/src/main/java/tools/dynamia/modules/importacion/ImportStatus.java
@@ -0,0 +1,64 @@
+package tools.dynamia.modules.importacion;
+
+import tools.dynamia.domain.Descriptor;
+import tools.dynamia.domain.util.DomainUtils;
+
+import java.io.Serializable;
+
+@Descriptor(fields = {"row", "imported", "message", "entityId", "entityName"})
+public class ImportStatus implements Serializable {
+
+    private long row;
+    private boolean imported;
+    private String message;
+    private Serializable entityId;
+    private String entityName;
+    private Object entity;
+
+    public ImportStatus(long row, boolean imported, String message, Object entity) {
+        this.row = row;
+        this.imported = imported;
+        this.message = message;
+        this.entity = entity;
+        this.entityId = DomainUtils.getJPAIdValue(entity);
+        this.entityName = entity.toString();
+    }
+
+    public ImportStatus(long row, boolean imported, String message, Serializable entityId, String entityName) {
+        this.row = row;
+        this.imported = imported;
+        this.message = message;
+        this.entityId = entityId;
+        this.entityName = entityName;
+    }
+
+    public ImportStatus(long row, boolean imported, String message) {
+        this.row = row;
+        this.imported = imported;
+        this.message = message;
+    }
+
+    public long getRow() {
+        return row;
+    }
+
+    public boolean isImported() {
+        return imported;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Serializable getEntityId() {
+        return entityId;
+    }
+
+    public String getEntityName() {
+        return entityName;
+    }
+
+    public Object getEntity() {
+        return entity;
+    }
+}
