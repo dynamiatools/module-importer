d77ab81d32c59d1d4c34e6b7ad3c1580f744bc11
Mario Serrano <mario@dynamiasoluciones.com>
2020-10-16 03:20:30 -0500

    new version and upgrade to DynamiaTools v4.0.0.Final

--- sources/ui/src/main/java/tools/dynamia/modules/importer/ImportOperation.java
+++ sources/ui/src/main/java/tools/dynamia/modules/importer/ImportOperation.java
@@ -1,7 +1,6 @@
 package tools.dynamia.modules.importer;
 
-import org.apache.commons.lang.time.DurationFormatUtils;
-
+import tools.dynamia.commons.StringUtils;
 import tools.dynamia.domain.ValidationError;
 import tools.dynamia.integration.ProgressEvent;
 import tools.dynamia.integration.ProgressMonitor;
@@ -13,151 +12,151 @@ import tools.dynamia.zk.util.LongOperation;
 
 public abstract class ImportOperation extends LongOperation implements ProgressMonitorListener {
 
-	private ProgressMonitor monitor;
-	private Importer importer;
-	private String name;
-	private long updateProgressRate = 3000;
-	private long lastCheckTime;
-	private long currentTime;
-	private boolean cancelledGracefully;
-	private long startTime = 0;
+    private ProgressMonitor monitor;
+    private Importer importer;
+    private String name;
+    private long updateProgressRate = 3000;
+    private long lastCheckTime;
+    private long currentTime;
+    private boolean cancelledGracefully;
+    private long startTime = 0;
 
-	public ImportOperation(String name, Importer importer) {
-		super();
-		this.name = name;
-		this.importer = importer;
-		init();
-	}
+    public ImportOperation(String name, Importer importer) {
+        super();
+        this.name = name;
+        this.importer = importer;
+        init();
+    }
 
-	public ImportOperation(String name, Importer importer, long updateProgressRate) {
-		super();
-		this.name = name;
-		this.importer = importer;
-		this.updateProgressRate = updateProgressRate;
-		init();
-	}
+    public ImportOperation(String name, Importer importer, long updateProgressRate) {
+        super();
+        this.name = name;
+        this.importer = importer;
+        this.updateProgressRate = updateProgressRate;
+        init();
+    }
 
-	private void init() {
-		execute(this::doImport);
-		onFinish(this::importFinish);
-	}
+    private void init() {
+        execute(this::doImport);
+        onFinish(this::importFinish);
+    }
 
-	@Override
-	public void progressChanged(ProgressEvent evt) {
-		try {
+    @Override
+    public void progressChanged(ProgressEvent evt) {
+        try {
 
-			currentTime = System.currentTimeMillis();
+            currentTime = System.currentTimeMillis();
 
-			if (lastCheckTime == 0) {
-				lastCheckTime = currentTime;
-			}
+            if (lastCheckTime == 0) {
+                lastCheckTime = currentTime;
+            }
 
-			long elapsedTime = currentTime - startTime;
-			if ((currentTime - lastCheckTime) >= updateProgressRate) {
-				lastCheckTime = currentTime;
+            long elapsedTime = currentTime - startTime;
+            if ((currentTime - lastCheckTime) >= updateProgressRate) {
+                lastCheckTime = currentTime;
 
-				double position = monitor.getCurrent();
-				double total = monitor.getMax();
+                double position = monitor.getCurrent();
+                double total = monitor.getMax();
 
-				if (startTime == 0) {
-					startTime = currentTime;
-				}
+                if (startTime == 0) {
+                    startTime = currentTime;
+                }
 
-				long estimatedRemaining = (long) (elapsedTime / position * (total - position));
+                long estimatedRemaining = (long) (elapsedTime / position * (total - position));
 
-				activate();
-				monitor.setMessage(monitor.getMessage() + " - Faltan <b>"
-						+ DurationFormatUtils.formatDuration(estimatedRemaining, "HH:mm:ss") + "</b> (h:m:s)");
-				importer.updateProgress(monitor);
-				deactivate();
-			}
-		} catch (Exception e) {
-			// Ignore
+                activate();
+                monitor.setMessage(monitor.getMessage() + " - Faltan <b>"
+                        + StringUtils.formatDuration(estimatedRemaining) + "</b> (h:m:s)");
+                importer.updateProgress(monitor);
+                deactivate();
+            }
+        } catch (Exception e) {
+            // Ignore
 
-		}
-	}
+        }
+    }
 
-	private void doImport() {
-		monitor = new ProgressMonitor(this);
-		try {
-			if (checkCurrentOperation()) {
+    private void doImport() {
+        monitor = new ProgressMonitor(this);
+        try {
+            if (checkCurrentOperation()) {
 
-				importer.setCurrentOperation(this);
-				setOperationStatus(true);
+                importer.setCurrentOperation(this);
+                setOperationStatus(true);
 
-				execute(monitor);
+                execute(monitor);
 
-				importer.setCurrentOperation(null);
-				setOperationStatus(false);
-			} else {
-				setOperationStatus(false);
-				cancel();
-			}
-		} catch (InterruptedException e) {
-			throw new ImportOperationException(e);
-		} catch (ValidationError e) {
-			try {
-				activate();
-				UIMessages.showMessage(e.getMessage(), MessageType.ERROR);
-				importer.setOperationStatus(false);
-				deactivate();
-			} catch (Exception e2) {
-				// TODO: handle exception
-			}
-		} catch (Exception e) {
-			throw new ImportOperationException(e);
-		}
-	}
+                importer.setCurrentOperation(null);
+                setOperationStatus(false);
+            } else {
+                setOperationStatus(false);
+                cancel();
+            }
+        } catch (InterruptedException e) {
+            throw new ImportOperationException(e);
+        } catch (ValidationError e) {
+            try {
+                activate();
+                UIMessages.showMessage(e.getMessage(), MessageType.ERROR);
+                importer.setOperationStatus(false);
+                deactivate();
+            } catch (Exception e2) {
+                // TODO: handle exception
+            }
+        } catch (Exception e) {
+            throw new ImportOperationException(e);
+        }
+    }
 
-	public void cancelGracefully() {
-		cancelledGracefully = true;
-		monitor.setMax(-1);
-		monitor.setCurrent(-1);
-		monitor.stop();
-	}
+    public void cancelGracefully() {
+        cancelledGracefully = true;
+        monitor.setMax(-1);
+        monitor.setCurrent(-1);
+        monitor.stop();
+    }
 
-	private void importFinish() {
-		if (!cancelledGracefully) {
-			onFinish(monitor);
-		} else {
-			onCancel(monitor);
-		}
-	}
+    private void importFinish() {
+        if (!cancelledGracefully) {
+            onFinish(monitor);
+        } else {
+            onCancel(monitor);
+        }
+    }
 
-	protected void onFinish(ProgressMonitor monitor) {
-		// TODO Auto-generated method stub
-	}
+    protected void onFinish(ProgressMonitor monitor) {
+        // TODO Auto-generated method stub
+    }
 
-	protected void onCancel(ProgressMonitor monitor) {
-		// TODO Auto-generated method stub
+    protected void onCancel(ProgressMonitor monitor) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	private void setOperationStatus(boolean b) {
-		try {
-			activate();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		importer.setOperationStatus(b);
-		deactivate();
-	}
+    private void setOperationStatus(boolean b) {
+        try {
+            activate();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        importer.setOperationStatus(b);
+        deactivate();
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	private boolean checkCurrentOperation() {
-		if (importer.getCurrentOperation() != null) {
-			UIMessages.showMessage("No puede ejecutar este proceso, existe una operacion de importacion activa "
-					+ importer.getCurrentOperation().getName(), null);
-			return false;
-		} else {
-			return true;
-		}
-	}
+    private boolean checkCurrentOperation() {
+        if (importer.getCurrentOperation() != null) {
+            UIMessages.showMessage("No puede ejecutar este proceso, existe una operacion de importacion activa "
+                    + importer.getCurrentOperation().getName(), null);
+            return false;
+        } else {
+            return true;
+        }
+    }
 
-	public abstract void execute(ProgressMonitor monitor) throws Exception;
+    public abstract void execute(ProgressMonitor monitor) throws Exception;
 
 }
